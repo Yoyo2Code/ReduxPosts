2709d7402008e54d1223ed3d88258792ce936da6
Yoseph Anderson <yrganderson@gmail.com>
2016-12-24 13:05:22 -0700

    post form * validates inputs * link to cancel and go to root path

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
+import { Link } from 'react-router';
+
 class PostsIndex extends Component {
     componentWillMount() {
       this.props.fetchPosts();
@@ -11,7 +13,14 @@ class PostsIndex extends Component {
 
     render() {
         return (
-          <div>List of blog posts</div>
+          <div>
+            <div className="text-xs-right">
+                <Link to="posts/new" className="btn btn-primary">
+                  Add a Post
+                </Link>
+            </div>
+              List of blog posts
+          </div>
         );
     }
 }
---
+++ src/components/posts_new.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsNew extends Component {
+
+    _checkIfValid(prop) {
+        return(
+            `form-group ${prop.touched && prop.invalid ? 'has-danger' : ''}`
+        );
+    }
+
+    render() {
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+              <h3>Create A New Post</h3>
+              <div className={this._checkIfValid(title)}>
+                <label>Title</label>
+                <input type="text" className="form-control" {...title} />
+                  <div className="text-help">
+                    {title.touched ? title.error : ''}
+                  </div>
+              </div>
+
+              <div className={this._checkIfValid(categories)}>
+                <label>Categories</label>
+                <input type="text" className="form-control" {...categories}/>
+                  <div className="text-help">
+                    {categories.touched ? categories.error : ''}
+                  </div>
+              </div>
+
+              <div className={this._checkIfValid(content)}>
+                <label>Content</label>
+                <textarea type="text" className="form-control" {...content}/>
+                  <div className="text-help">
+                    {content.touched ? content.error : ''}
+                  </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary">Submit</button>
+              <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = "Enter a username";
+    }
+
+    if(!values.categories) {
+        errors.categories = "Enter a category"
+    }
+
+    if(!values.content) {
+        errors.content = "Enter some content"
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form: 'PostsNew',
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, { createPost })(PostsNew);
