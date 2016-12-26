93c2d548146d63d492f7121ed72a233cdee0520a
Yoseph Anderson <yrganderson@gmail.com>
2016-12-25 23:38:33 -0700

    post index * display all posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=ninjasarecoolerthanyours';
+const API_KEY = '?key=ineededanewkeytoo';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -16,7 +16,7 @@ export function fetchPosts() {
 }
 
 export function createPost(props) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`)
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
 
     return {
         type: CREATE_POST,
