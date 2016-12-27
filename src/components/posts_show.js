b00aad8254988091d1e0c1a27694fbfa1a50adc8
Yoseph Anderson <yrganderson@gmail.com>
2016-12-27 10:33:39 -0700

    post show page *displays post data * link back to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
     render() {
+
+        const { post } = this.props;
+        if(!post) {
+            return(
+                <div>Loading...</div>
+            );
+        }
+
         return(
-            <div>Show post {this.props.params.id} </div>
+            <div>
+                <Link to='/'>Back to Index</Link>
+                <h3>{post.title}</h3>
+                <h6>{post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
         );
     }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+    return {
+        post: state.posts.post
+    };
+}
+
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
