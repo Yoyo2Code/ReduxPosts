dc437b10333a8990f9a247cbf27b65e614eea769
Yoseph Anderson <yrganderson@gmail.com>
2016-12-26 22:47:45 -0700

    post show page

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostIndex} />
         <Route path="posts/new" component={PostsNew} />
+        <Route path="posts/:id" component={PostsShow} />
     </Route>
 );
